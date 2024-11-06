67b39633ba64e78b19ea83ca27397ab372d74773
Pieterjan Spoelders <pj.spoelders@gmail.com>
2024-11-06 09:26:36 -0500

    add ExasolPool class

---
+++ src/lib/sql-pool.ts
@@ -0,0 +1,105 @@
+import { createPool, Factory, Pool } from 'generic-pool';
+import { Config, ExasolDriver, websocketFactory } from './sql-client';
+import { ILogger, LogLevel, Logger } from './logger/logger';
+import { QueryResult } from './query-result';
+import { Attributes } from './commands';
+import { CetCancelFunction } from './sql-client.interface';
+import { SQLQueriesResponse, SQLResponse } from './types';
+//https://www.npmjs.com/package/generic-pool
+//https://www.npmjs.com/package/@types/generic-pool
+export interface PoolConfig {
+  min: number;
+  max: number;
+}
+export function getPool(websocketFactory: websocketFactory, config: Partial<Config> & Partial<PoolConfig>, logger: ILogger) {
+  // export interface Factory<T> {
+  //   create(): Promise<T>;
+  //   destroy(client: T): Promise<void>;
+  //   validate?(client: T): Promise<boolean>;
+  // }
+  async function createClient() {
+    const exasolClient: ExasolDriver = new ExasolDriver(websocketFactory, config, logger);
+    await exasolClient.connect();
+    return Promise.resolve(exasolClient);
+  }
+
+  async function destroyClient(exasolClient: ExasolDriver) {
+    await exasolClient.close();
+  }
+
+  const poolFactory: Factory<ExasolDriver> = {
+    create: function () {
+      return createClient();
+    },
+    destroy: function (client: ExasolDriver) {
+      return destroyClient(client);
+    },
+  };
+  const poolOpts: PoolConfig = {
+    max: config.max ?? 1, // maximum size of the pool
+    min: config.min ?? 0, // minimum size of the pool
+  };
+  const tempPool = createPool(poolFactory, poolOpts);
+  return tempPool;
+}
+//TODO:add getters and setters,refer to pool documentation
+export class ExasolPool {
+  private internalPool: Pool<ExasolDriver>;
+  private logger: ILogger;
+  constructor(
+    websocketFactory: websocketFactory,
+    config: Partial<Config> & Partial<PoolConfig>,
+    logger: ILogger = new Logger(LogLevel.Debug),
+  ) {
+    this.logger = logger;
+    this.internalPool = getPool(websocketFactory, config, logger);
+  }
+
+  async query(
+    sqlStatement: string,
+    attributes?: Partial<Attributes> | undefined,
+    getCancel?: CetCancelFunction | undefined,
+  ): Promise<QueryResult>;
+  async query(
+    sqlStatement: string,
+    attributes?: Partial<Attributes> | undefined,
+    getCancel?: CetCancelFunction | undefined,
+    responseType?: 'default' | undefined,
+  ): Promise<QueryResult>;
+  async query(
+    sqlStatement: string,
+    attributes?: Partial<Attributes> | undefined,
+    getCancel?: CetCancelFunction | undefined,
+    responseType?: 'raw' | undefined,
+  ): Promise<SQLResponse<SQLQueriesResponse>>;
+  async query(
+    sqlStatement: string,
+    attributes?: Partial<Attributes> | undefined,
+    getCancel?: CetCancelFunction | undefined,
+    responseType?: 'default' | 'raw',
+  ): Promise<QueryResult | SQLResponse<SQLQueriesResponse>> {
+    {
+      let exasolClient;
+      try {
+        //TODO: add error handling for maxWaitingClients?
+        exasolClient = await this.internalPool.acquire();
+
+        const response = await exasolClient.query(sqlStatement, attributes, getCancel, responseType);
+        await this.internalPool.release(exasolClient);
+        return response;
+      } catch (err) {
+        if (exasolClient) {
+          await this.internalPool.release(exasolClient);
+        }
+        this.logger.log('Query method error:' + err);
+        throw err;
+      }
+    }
+  }
+  async drain() {
+    await this.internalPool.drain();
+  }
+  async clear() {
+    await this.internalPool.clear();
+  }
+}
