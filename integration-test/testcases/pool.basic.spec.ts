d61785146b0bd15e60f9ecf54627c17029d69a59
Pieterjan Spoelders <pj.spoelders@gmail.com>
2024-11-07 08:16:29 -0500

    added a test for ExasolPool for x(20) # of requests

--- integration-test/testcases/pool.basic.spec.ts
+++ integration-test/testcases/pool.basic.spec.ts
@@ -2,6 +2,7 @@ import { GenericContainer, StartedTestContainer, Wait } from 'testcontainers';
 import { ExasolDriver, websocketFactory } from '../../src/lib/sql-client';
 import { ExasolPool } from '../../src/lib/sql-pool';
 import { RandomUuid } from 'testcontainers/dist/uuid';
+import { QueryResult } from '../../src/lib/query-result';
 
 export const basicPoolTests = (name: string, factory: websocketFactory) =>
   describe(name, () => {
@@ -37,6 +38,45 @@ export const basicPoolTests = (name: string, factory: websocketFactory) =>
       });
       await poolToQuery.drain();
       await poolToQuery.clear();
+    });
+
+    it('Exec and fetch', async () => {
+      const setupClient = new ExasolDriver(factory, {
+        host: container.getHost(),
+        port: container.getMappedPort(8563),
+        user: 'sys',
+        password: 'exasol',
+        encryption: false,
+      });
+
+      const poolToQuery = new ExasolPool(factory, {
+        host: container.getHost(),
+        port: container.getMappedPort(8563),
+        user: 'sys',
+        password: 'exasol',
+        encryption: false,
+        min: 1,
+        max: 10,
+      });
+
+      await setupClient.connect();
+
+      await setupClient.execute('CREATE SCHEMA ' + schemaName);
+      await setupClient.execute('CREATE TABLE ' + schemaName + '.TEST_TABLE(x INT)');
+      await setupClient.execute('INSERT INTO ' + schemaName + '.TEST_TABLE VALUES (15)');
+
+      const data = await poolToQuery.query('SELECT x FROM ' + schemaName + '.TEST_TABLE');
+
+      expect(data.getColumns()[0].name).toBe('X');
+      expect(data.getRows()[0]['X']).toBe(15);
+
+      // poolToQuery.drain().then(function () {
+      //   poolToQuery.clear();
+      // });
+      await poolToQuery.drain();
+      await poolToQuery.clear();
+
+      await setupClient.close();
     });
 
     it('Fetch multiple queries simultaneously/asynchronously', async () => {
@@ -91,7 +131,7 @@ export const basicPoolTests = (name: string, factory: websocketFactory) =>
       await setupClient.close();
     });
 
-    it('Exec and fetch', async () => {
+    it('Fetch multiple queries asynchronously (high volume)', async () => {
       const setupClient = new ExasolDriver(factory, {
         host: container.getHost(),
         port: container.getMappedPort(8563),
@@ -115,89 +155,29 @@ export const basicPoolTests = (name: string, factory: websocketFactory) =>
       await setupClient.execute('CREATE SCHEMA ' + schemaName);
       await setupClient.execute('CREATE TABLE ' + schemaName + '.TEST_TABLE(x INT)');
       await setupClient.execute('INSERT INTO ' + schemaName + '.TEST_TABLE VALUES (15)');
+      const promiseArr: Promise<QueryResult>[] = [];
+      const amountOfRequests = 20;
 
-      const data = await poolToQuery.query('SELECT x FROM ' + schemaName + '.TEST_TABLE');
+      for (let i = 0; i < amountOfRequests; i++) {
+        const dataPromise = poolToQuery.query('SELECT x FROM ' + schemaName + '.TEST_TABLE');
+        promiseArr.push(dataPromise);
+      }
 
-      expect(data.getColumns()[0].name).toBe('X');
-      expect(data.getRows()[0]['X']).toBe(15);
+      await Promise.all(promiseArr);
 
-      // poolToQuery.drain().then(function () {
-      //   poolToQuery.clear();
-      // });
+      for (let i = 0; i < amountOfRequests; i++) {
+        const data = await promiseArr[i];
+        expect(data.getColumns()[0].name).toBe('X');
+        expect(data.getRows()[0]['X']).toBe(15);
+      }
+
       await poolToQuery.drain();
       await poolToQuery.clear();
 
       await setupClient.close();
     });
 
-    // it('Exec and fetch (raw)', async () => {
-    //   const driver = await openConnection(factory, container);
-
-    //   await driver.execute('CREATE SCHEMA ' + schemaName, undefined, undefined, 'raw');
-    //   await driver.execute('CREATE TABLE ' + schemaName + '.TEST_TABLE(x INT)');
-    //   await driver.execute('INSERT INTO ' + schemaName + '.TEST_TABLE VALUES (15)');
-
-    //   const data = await driver.execute('SELECT x FROM ' + schemaName + '.TEST_TABLE', undefined, undefined, 'raw');
-
-    //   expect(data.status).toBe('ok');
-    //   expect(data.responseData.numResults).toBe(1);
-    //   expect(data.responseData.results[0].resultType).toBe('resultSet');
-    //   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    //   expect(data.responseData.results[0].resultSet?.data![0][0]).toBe(15);
-
-    //   await driver.close();
-    // });
-
-    // it('Fetch', async () => {
-    //   const driver = await openConnection(factory, container);
-
-    //   await driver.execute('CREATE SCHEMA ' + schemaName);
-    //   await driver.execute('CREATE TABLE ' + schemaName + '.TEST_TABLE(x INT)');
-    //   const exampleData: string[] = [];
-
-    //   for (let index = 0; index < 10000; index++) {
-    //     exampleData.push(`(${index})`);
-    //   }
-
-    //   await driver.execute('INSERT INTO ' + schemaName + '.TEST_TABLE VALUES ' + exampleData.join(','));
-    //   const data = await driver.query('SELECT x FROM ' + schemaName + '.TEST_TABLE GROUP BY x ORDER BY x');
-
-    //   expect(data.getRows()).toHaveLength(10000);
-    //   await driver.close();
-    // });
-
-    // afterEach(async () => {
-    //   if (tmpDriver) {
-    //     try {
-    //       await tmpDriver?.close();
-    //     } catch (error) {
-    //       console.log('Could not close driver');
-    //     }
-    //   }
-
-    //   try {
-    //     const driver = await openConnection(factory, container);
-    //     await driver.execute('DROP SCHEMA IF EXISTS ' + schemaName + ' CASCADE');
-    //     await driver.close();
-    //   } catch (error) {
-    //     console.log('Could not cleanup schema', schemaName);
-    //   }
-    // });
-
     afterAll(async () => {
       //  await container.stop();
     });
-
-    // const openConnection = async (factory: websocketFactory, container: StartedTestContainer) => {
-    //   const driver = new ExasolClient(factory, {
-    //     host: container.getHost(),
-    //     port: container.getMappedPort(8563),
-    //     user: 'sys',
-    //     password: 'exasol',
-    //     encryption: false,
-    //   });
-    //   await driver.connect();
-    //   tmpDriver = driver;
-    //   return driver;
-    // };
   });
