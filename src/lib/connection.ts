e8e062bd303b3df361e4f167b2f61ed8c5f79ec0
Pieterjan Spoelders <pj.spoelders@gmail.com>
2025-03-11 05:26:43 -0400

    format connection.ts

--- src/lib/connection.ts
+++ src/lib/connection.ts
@@ -4,7 +4,7 @@ import { PoolItem } from './pool/pool';
 import { ILogger } from './logger/logger';
 import { ErrClosed, ErrInvalidConn, ErrJobAlreadyRunning, ErrMalformedData } from './errors/errors';
 import { AbortQueryCommand, Commands, CommandsNoResult, DisconnectCommand } from './commands';
-import {deflate,inflate} from 'pako';
+import { deflate, inflate } from 'pako';
 export interface ExaMessageEvent {
   data: unknown;
   type: string;
@@ -36,7 +36,7 @@ export class Connection implements PoolItem {
   private isBroken = false;
   private useCompression = false;
 
-  public setCompression(compression: boolean){
+  public setCompression(compression: boolean) {
     this.useCompression = compression;
   }
   public set active(v: boolean) {
@@ -54,7 +54,11 @@ export class Connection implements PoolItem {
   public get broken(): boolean {
     return this.isBroken;
   }
-  constructor(private readonly websocket: ExaWebsocket, private readonly logger: ILogger, public name: string) {
+  constructor(
+    private readonly websocket: ExaWebsocket,
+    private readonly logger: ILogger,
+    public name: string,
+  ) {
     this.websocket = websocket;
     this.logger = logger;
     this.name = name;
@@ -98,11 +102,11 @@ export class Connection implements PoolItem {
     const cmdStr: string = JSON.stringify(cmd);
 
     if (this.useCompression) {
-      this.logger.debug("Using compression");
+      this.logger.debug('Using compression');
       const deflatedData = this.encodeAndCompressData(cmdStr);
       this.connection.send(deflatedData);
     } else {
-      this.logger.debug("Not using compression");
+      this.logger.debug('Not using compression');
       this.connection.send(cmdStr);
     }
   }
@@ -112,80 +116,75 @@ export class Connection implements PoolItem {
       this.isBroken = true;
       return Promise.reject(ErrClosed);
     }
-    this.logger.debug("Entered sendCommand");
+    this.logger.debug('Entered sendCommand');
     const cancelQuery = () => {
       this.sendCommandWithNoResult(new AbortQueryCommand());
     };
     this.logger.debug(`[useCompression is: ${this.useCompression}]`);
 
     getCancel && getCancel(cancelQuery);
-    
-    return new Promise<SQLResponse<T>>((resolve, reject) => {
 
+    return new Promise<SQLResponse<T>>((resolve, reject) => {
       if (this.connection === undefined) {
         this.isBroken = true;
         reject(ErrInvalidConn);
       } else {
-
         this.connection.onmessage = (event) => {
-          
           try {
             this.logger.debug(`[Entered OnMessage for :${this.name}]`);
             this.logger.debug(`[Cmd sent was: ${JSON.stringify(cmd)}]`);
             this.logger.debug(`[Compression enabled: ${this.useCompression}]`);
 
-          this.active = false;
-          let data :SQLResponse<T>;
-          if (this.useCompression) {
-            
-            this.logger.debug("inflate");
-            const decompressed = inflate(new Uint8Array(event.data));
-            this.logger.debug("decode")
-            const decoded  = new TextDecoder().decode(decompressed);
-            
-            this.logger.debug("parse");
-            data = JSON.parse(decoded) as SQLResponse<T> ;
-          } else {
-            data = JSON.parse(event.data) as SQLResponse<T>;
-          }
-          this.logger.debug(`[Connection:${this.name}] Received data`);
+            this.active = false;
+            let data: SQLResponse<T>;
+            if (this.useCompression) {
+              this.logger.debug('inflate');
+              const decompressed = inflate(new Uint8Array(event.data));
+              this.logger.debug('decode');
+              const decoded = new TextDecoder().decode(decompressed);
 
-          if (data.status !== 'ok') {
-            this.logger.debug(`[Connection:${this.name}] Received invalid data or error`);
-
-            if (data.exception) {
-              resolve(data);
+              this.logger.debug('parse');
+              data = JSON.parse(decoded) as SQLResponse<T>;
             } else {
-              reject(ErrMalformedData);
+              data = JSON.parse(event.data) as SQLResponse<T>;
             }
+            this.logger.debug(`[Connection:${this.name}] Received data`);
 
-            return;
-          }
-          resolve(data);
-        } catch (error: unknown) {
-          let errorMessage = "Unexpected error in message handling";
-          if (error instanceof Error) {
+            if (data.status !== 'ok') {
+              this.logger.debug(`[Connection:${this.name}] Received invalid data or error`);
+
+              if (data.exception) {
+                resolve(data);
+              } else {
+                reject(ErrMalformedData);
+              }
+
+              return;
+            }
+            resolve(data);
+          } catch (error: unknown) {
+            let errorMessage = 'Unexpected error in message handling';
+            if (error instanceof Error) {
               errorMessage = error.message;
+            }
+            console.log(`[Unhandled error in onmessage: ${errorMessage}]`);
+            reject(new Error(errorMessage));
           }
-          console.log(`[Unhandled error in onmessage: ${errorMessage}]`);
-          reject(new Error(errorMessage));
-      }
         };
         //end of onMessage
 
         this.connection.onerror = (event: unknown) => {
-          this.logger.trace("WebSocket error:", event);
-      };
+          this.logger.trace('WebSocket error:', event);
+        };
 
-      //sendCommand 'resumes'
+        //sendCommand 'resumes'
         if (this.active === true) {
           reject(ErrJobAlreadyRunning);
           return;
         }
         this.logger.debug(`[Connection:${this.name}] Send request:`, cmd);
         this.sendCmd(cmd);
-
       }
     }); //end of return new promise
-  }//end of sendCommand
+  } //end of sendCommand
 }
