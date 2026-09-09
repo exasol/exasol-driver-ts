d67fb190720e6e9fa3ed0ed04040da30a51edc82
Christoph Pirkl <christoph.pirkl@exasol.com>
2026-09-09 16:27:36 +0200

    Add clarification in JSDoc

--- src/lib/node-sql-pool.ts
+++ src/lib/node-sql-pool.ts
@@ -3,7 +3,7 @@ import { NodeExasolDriver } from './node-sql-client';
 import { Config, WebsocketFactory } from './sql-client';
 import { BaseExasolPool, ClientPoolConfig } from './sql-pool';
 
-/** Node.js connection pool with CSV-capable drivers. */
+/** Node.js connection pool that creates CSV-capable {@link NodeExasolDriver} instances. */
 export class NodeExasolPool extends BaseExasolPool<NodeExasolDriver> {
   constructor(
     websocketFactory: WebsocketFactory,
